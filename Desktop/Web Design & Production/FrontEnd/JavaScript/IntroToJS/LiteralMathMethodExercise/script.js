ec0d5f264b5b35fdea154c1f5a4621786805e314
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-02-01 10:33:51 -0600

    Exercise completed

---
+++ Desktop/Web Design & Production/FrontEnd/JavaScript/IntroToJS/LiteralMathMethodExercise/script.js
@@ -0,0 +1,14 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs);
+console.log(warmHugs.toUpperCase)
+console.log(warmHugs.replace ("like", "love"))
+var beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice (18));
+var dotDotDot = ". . ."
+var skullBones = `I dont have a skull ${dotDotDot} or bones.`
+console.log(skullBones)
+console.log(Math.PI);
+var randomNumber = Math.random() *3 +1
+randomNumber = Math.floor (randomNumber)
+console.log(randomNumber);
+
